e8ae91d769c9ea83a2b27764e4f92ba81c0f08c3
asiamah <54848608+infawikirich@users.noreply.github.com>
2022-02-23 17:51:48 +0000

    Updated the repository with circle_spiral.py

---
+++ circle_spiral.py
@@ -0,0 +1,19 @@
+import turtle
+
+
+win = turtle.Screen()
+win.title("Circle Spiral")
+
+
+turtle.speed("fastest")
+turtle.pensize(5)
+
+
+
+for k in range(100):
+  turtle.circle(100)
+  turtle.right(91)
+  
+  
+  
+ turtle.mainloop()
