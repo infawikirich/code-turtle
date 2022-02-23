58e8eed7777c15821df90902aec0dc607143d3aa
infallible <infawikirich@gmail.com>
2022-02-23 23:56:38 +0000

    Fill update of random_island_generator.py

--- random_island_generator.py
+++ random_island_generator.py
@@ -13,6 +13,7 @@ turtle.pencolor('green')
 turtle.speed(0)
 turtle.tracer(0, 0)
 turtle.hideturtle()
+turtle.fillcolor('forest green')
 
 
 def draw_line(x1, y1, x2, y2):
@@ -31,10 +32,24 @@ def shoreline(x1, y1, x2, y2, ratio):
         draw_line(x1, y1, x2, y2)
         return
 
+    rs = ratio + random.uniform(-0.1, 0.1)   # let ratio fluctuate slightly around the chosen value
+    rs = max(0.5, rs)    # make sure ratio stays at least half of the length
+    midx = (x1 + x2)/2 # center of ellipse
+    midy = (y1 + y2)/2
+    rx = L/2 + (2 * rs - 1)/2*L   # width of ellipse
+    ry = ((L*rs)**2 - (L/2)**2)**0.5   # height of ellipse
+    theta = math.atan2(y2 - y1, x2 - x1)   # the tilt angle of ellipse
+    alpha = random.uniform(math.pi*0.3,math.pi*0.7)   # flucuate around math.pi/2
+    x3 = rx*math.cos(alpha)*math.cos(theta) - ry*math.sin(alpha)*math.sin(theta) + midx # parmetric equation for ellipse
+    y3 = rx*math.cos(alpha)*math.sin(theta) - ry*math.sin(alpha)*math.cos(theta) + midy
+    shoreline(x1, y1, x3, y3, ratio)   # do this recursively on each segment
+    shoreline(x3, y3, x2, y2, ratio)
 
 
-
-
+turtle.begin_fill()
+shoreline(-300, 0, 300, 0, 0.55)   # call recursion
+shoreline(300, 0, -300, 0, 0.55)   # call recursion
+turtle.end_fill()
 
 
 turtle.mainloop()
